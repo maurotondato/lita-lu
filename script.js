40ededee01e17e27e6c79fb78fe249c1206082f9
maurotondato <maurotondato@MacBook-Pro-de-Mauro.local>
2024-05-13 23:48:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Script para desplazamiento suave al hacer clic en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var banner = document.getElementById("banner");
+    banner.classList.add("loaded");
+});
